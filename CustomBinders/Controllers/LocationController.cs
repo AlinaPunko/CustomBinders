926756cbd69e00a149179721bd8d7fcde8e2486b
punko.alina2000 <punko.alina2000@gmail.com>
2020-02-07 14:47:38 +0300

    Add project files.

---
+++ CustomBinders/Controllers/LocationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomBinders.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index(Point point)
+        {
+            return Json(point);
+        }
+    }
+}
---
+++ CustomBinders/Models/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomBinders.Models
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
